dde34f397661d12b0738baced36f03b6ede6a4d7
Christophe Guillon <christophe.guillon@st.com>
2018-10-16 19:30:48 +0200

    Add check for incorrect symbolic ref in HEAD

--- tests/test014.sh
+++ tests/test014.sh
@@ -35,24 +35,33 @@ pushd test-1
 $REPO_MIRROR -m "$TMPTEST/repo-mirrors" -d -q -- init -u file://"$TMPTEST"/repos/manifests.git </dev/null
 popd
 
-# Corrupt HEAD on the mirror, has it may happend when a manifest contains
-# incorrect revisions or repo init is interrupted
+# Corrupt HEAD on the mirror, inject incorrect version (a sha1 with trailing chars)
 echo "da39a3ee5e6b4b0d3255bfef95601890afd80709X" | tee repo-mirrors/default/repos/project1.git/HEAD
 
-# Run a second repo-mirror init, repo may fail to init on Invalid HEAD
+# Run a repo-mirror init, repo may fail to init on Invalid HEAD
 # repo-mirror corrects this error by setting a well formed HEAD reference before mirror sync
 mkdir test-2
 pushd test-2
 $REPO_MIRROR -m "$TMPTEST/repo-mirrors" -d -q -- init -u file://"$TMPTEST"/repos/manifests.git </dev/null
 popd
 
+# Corrupt HEAD on the mirror, inject incorect reference (a reference wjhich is a dir)
+echo "ref: refs/heads" | tee repo-mirrors/default/repos/project1.git/HEAD
+
+mkdir test-3
+pushd test-3
+$REPO_MIRROR -m "$TMPTEST/repo-mirrors" -d -q -- init -u file://"$TMPTEST"/repos/manifests.git </dev/null
+popd
+
+
 # Remove HEAD completly, this is invalid, though in this case, repo-mirror
 # does not attempt any recovery
 rm repo-mirrors/default/repos/project1.git/HEAD
-mkdir test-3
-pushd test-3
+
+mkdir test-4
+pushd test-4
 res=0
 $REPO_MIRROR -m "$TMPTEST/repo-mirrors" -d -q -- init -u file://"$TMPTEST"/repos/manifests.git </dev/null 2>&1 | tee error.log || res=$?
 popd
 # In this case normally repo should fail to recognize the mirrored repo as a git repository
-[ "$res" != 0 ] && grep -i -w -q "Not a git repository" test-3/error.log
+[ "$res" != 0 ] && grep -i -w -q "Not a git repository" test-4/error.log
